85d89918d792850d8ab78ba4498f5fe3434f252f
kiemnx <kiemnguyenxuan@gmail.com>
2020-06-10 21:25:08 +0700

    First commit

---
+++ src/lession1/HelloJava.java
@@ -0,0 +1,10 @@
+package lession1;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        System.out.println("Hello everyone");
+    }
+}
