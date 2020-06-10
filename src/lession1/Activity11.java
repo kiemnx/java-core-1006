6e808ba8310bd8b925736e279feb733e302ba132
kiemnx <kiemnguyenxuan@gmail.com>
2020-06-10 21:29:59 +0700

    Add new file code

---
+++ src/lession1/Activity11.java
@@ -0,0 +1,7 @@
+package lession1;
+
+public class Activity11 {
+    public static void main(String[] args) {
+        System.out.println("Start activity 1-1");
+    }
+}
--- src/lession1/HelloJava.java
+++ src/lession1/HelloJava.java
@@ -3,8 +3,7 @@ package lession1;
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
 
-        System.out.println("Hello everyone");
+        System.out.println("This is first Java project");
     }
 }
