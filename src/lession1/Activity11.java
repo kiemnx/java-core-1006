27a36294de2ec786205651e90f3f42412d33124d
kiemnx <kiemngyenxuan@gmail.com>
2020-10-06 21:00:07 +0700

    Update Activity11

--- src/lession1/Activity11.java
+++ src/lession1/Activity11.java
@@ -3,5 +3,6 @@ package lession1;
 public class Activity11 {
     public static void main(String[] args) {
         System.out.println("Start activity 1-1");
+        System.out.println("Second line");
     }
 }
